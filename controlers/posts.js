981e0058daa8448f3591218445ad6a89664bc7c5
Volodymyr <lesiyk47@gmail.com>
2021-08-08 19:38:46 +0300

    wow

--- controlers/posts.js
+++ controlers/posts.js
@@ -1,4 +1,5 @@
 import postMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => { 
     try {
@@ -21,5 +22,17 @@ export const createPost = async (req, res) => {
         res.status(201).json(newPostMessage );
     } catch (error) {
         res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send("No post with that id")
+    } else {
+        const updatedPost = await postMessage.findByIdAndUpdate(_id, post, { new: true });
+        res.json(updatedPost);
     }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getPosts, createPost } from '../controlers/posts.js';
+import { getPosts, createPost, updatePost } from '../controlers/posts.js';
 
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 
 export default router;
